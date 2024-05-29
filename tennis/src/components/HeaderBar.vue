4ac4f1700ae0cf159d8ee1aa29dc4ad445073f70
jason-g <a2012886@yahoo.com.tw>
2024-05-29 23:24:57 +0800

    0529

---
+++ tennis/src/components/HeaderBar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+<h1>123123123</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- tennis/src/views/HomeView.vue
+++ tennis/src/views/HomeView.vue
@@ -1,7 +1,36 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import HeaderBar from '@/components/HeaderBar.vue'
+
 </script>
 
 <template>
-<h1>test</h1>
-</template>
+    <div class="header">
+        <img  src="../assets/logoImage/2017年夏季世界大學運動會羽球比賽_LOGO.png" alt="">
+        <h1>Badminton</h1>
+        <h1 class="chineseName">揮汗如羽</h1>
+    </div>
+    <HeaderBar />
+    </template>
+    
+    <style lang="scss" scoped>
+    .header{
+        width: 100vw;
+        height: 20vh;
+        background-color:#0779E4;
+        padding: 0 15vw;
+        display: flex;
+        justify-content:baseline;
+        align-items: center;
+        img{
+            height: 90%;
+        }
+        h1{
+            font-weight: 300;
+        }
+        .chineseName{
+            font-family: "Noto Sans TC", sans-serif;
+            font-weight: 700;
+        }
+    }
+    </style>
+    
